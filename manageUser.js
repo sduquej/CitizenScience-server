90bd8df83a1a96a42944885da8b8e62e5275592e
Sebastián Duque <sduquej@gmail.com>
2015-03-30 17:02:58 +0100

    changes before Apr01 demo

--- manageUser.js
+++ manageUser.js
@@ -5,17 +5,18 @@
  module.exports = function (server, db, fs, config){
   //    unique index
   // TODO: remove, make it generic
-  db.appUsers.ensureIndex({
-    email: 1
-  }, {
-    unique: true
-  });
+  // db.appUsers.ensureIndex({
+  //   email: 1
+  // }, {
+  //   unique: true
+  // });
 
   // TODO: Instead of returning a list of data, return general stats
   // e.g.: total number of documents, documents in the last month
   // time elapsed since last contribution
   server.get('/api/v1/simpleForm/list', function (req, res, next) {
     db.appUsers.find({},{_id:0},function (err, list) {
+      console.log(JSON.stringify(list));
       res.writeHead(200, {
         'Content-Type': 'application/json; charset=utf-8'
       });
@@ -25,13 +26,18 @@
   });
 
   server.post('api/v1/simpleForm/register', function (req, res, next){
+    debugger;
     var userContribution = req.params;
     var file = req.files.file;      
 
     // If a file was uploaded get it to the configured upload directory
+    debugger;
     if( file ){
+      // Store the metadata about the uploaded file
+      userContribution.files = JSON.parse(userContribution.files);
+      
       fs.readFile(file.path, function(err, data){
-        if(err){
+        if(err){          
           res.writeHead(400, {
             'Content-Type': 'application/json; charset=utf-8'
           });
@@ -39,8 +45,7 @@
             error: err,
             message: 'An error ocurred when reading the uploaded file'
           }));
-        }         
-
+        }                 
         // Create a unique filename using the timestamp of the contribution
         // some characters are removed from the timestamp to avoid problems 
         // with filename restrictions in some OS's
@@ -49,32 +54,28 @@
           if (err) {
            res.end(JSON.stringify({
              error: err,
-             message: 'Unexpected error'
+             message: 'Unexpected error when writing file'
             }));            
           }
           console.log('File saved @ ' + filename);
         });        
         return next();
         });
-    }
+    }   
     
-    // Store the metadata about the uploaded file
-    userContribution.files = JSON.parse(userContribution.files);
     
     db.appUsers.insert(userContribution, function (err, dbObject){
-     if (err) {
-        // duplicate key error
-        // TODO: Remove this code, make it error generic
-       if (err.code == 11000) {
-         res.writeHead(400, {
-           'Content-Type': 'application/json; charset=utf-8'
-         });
-         res.end(JSON.stringify({
-           error: err,
-           message: 'A user with this email already exists'
-         }));
-       }
-     }else {
+     if (err) {                      
+      debugger;
+       res.writeHead(400, {
+         'Content-Type': 'application/json; charset=utf-8'
+       });
+       res.end(JSON.stringify({
+         error: err,
+         message: err.code + ' - could not insert in the db'
+       }));       
+     }else {   
+      debugger;   
        res.writeHead(200, {
          'Content-Type': 'application/json; charset=utf-8'
        });
